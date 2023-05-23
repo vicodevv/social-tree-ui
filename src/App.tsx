f614e0c62683870ade8cc129a4aa5a8b999eb5be
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-05-23 17:22:55 +0100

    Sign In form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import SignIn from './pages/SignIn/SignIn';
+import Admin from './pages/Admin/Admin';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <SignIn/>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Admin/Admin.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Admin.scss';
+
+
+const Admin = () => {
+    return (
+        <div className="Admin">
+        <h1>Admin</h1>
+        </div>
+    );
+};
+
+export default Admin;
